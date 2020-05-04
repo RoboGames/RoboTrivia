89f627d147bb50fb8419110d62a4dc1bcda94a98
Aleks <nikolic.aleks90@gmail.com>
2020-05-04 15:06:07 -0400

    Added pseudo code

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
-  render(){
-  return (
-    <header>
-        <h1>Robo Trivia Landing Page!</h1>
-    </header>
-  );
-  }
+    render(){
+    return (
+        <header>
+            <h1>Robo Trivia Landing Page!</h1>
+        </header>
+        );
+    }
 }
 
 export default Header;
