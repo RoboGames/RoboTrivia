897df1fcdea73c02ab260785bcac71c526640378
Aleks <nikolic.aleks90@gmail.com>
2020-05-04 13:04:24 -0400

    First commit! Basic setup files for our awesome react trivia app.

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render(){
+  return (
+    <header>
+        <h1>Robo Trivia Landing Page!</h1>
+    </header>
+  );
+  }
+}
+
+export default Header;
