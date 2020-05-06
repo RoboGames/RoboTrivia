22106c4e9b8cd9f8dd09e585519199ec9ac0eaef
jing yi (jane) yuan <jingyiyuan95@gmail.com>
2020-05-05 23:17:15 -0400

    successful api call, displayed question on the page

--- src/App.js
+++ src/App.js
@@ -2,20 +2,66 @@ import React, { Component } from 'react';
 import Header from './Components/Header';
 import './Styles/styles.scss';
 
+import axios from 'axios';
+
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      questionArray:[]
+    }
+  }
 
   callApi = (category, difficulty, numberOfPlayers) =>{
     console.log(category, difficulty, numberOfPlayers)
+    let numberOfQuestions = numberOfPlayers * 5
+    console.log(numberOfQuestions)
+    axios({
+      url: 'http://jservice.io/api/clues',
+      method:'GET',
+      params: {
+        category: category,
+        value:difficulty
+      }
+    }).then((response) => {
+      console.log(response);
+      this.setState({
+        questionArray:response.data
+      })
+
+    })
+  }
+
+  componentDidMount() {
+    this.callApi()
+
   }
 
   render(){
-  return (
-    <>
-      <Header
-      callApiFunc = {this.callApi}
-      />
-    </>
-  );
+    return (
+      <div className ="App">
+          <Header
+            callApiFunc = {this.callApi}
+          />
+          <main>
+            <ul>
+              {this.state.questionArray.map((question,i)=>{
+                let questionTitle = question.question
+                let answer = question.answer
+                    return (
+                      <li className="questionContainer" key={i}>
+                        <h2>{questionTitle}</h2>
+                        <form action="">
+                          <input type="text"/>
+                        </form>
+                      </li>
+                    )
+
+              })}
+            </ul>
+          </main>
+      </div>
+    );
   }
 }
 
