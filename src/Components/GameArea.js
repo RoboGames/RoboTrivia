37773c0b373e387d68b9997448867b55b5e50fea
jing yi (jane) yuan <jingyiyuan95@gmail.com>
2020-05-07 15:19:26 -0400

    display question one by one on the screen

--- src/Components/GameArea.js
+++ src/Components/GameArea.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react'
 class GameArea extends Component {
+    constructor() {
+        super();
+        this.state = {
+            currentPlayer: '',
+            index: 0
+        }
+    }
+    nextQuestion = () => {
+        this.setState({
+            index: (this.state.index + 1)
+        })
+    }
     render() {
+        console.log(this.props.renderQuestions[0])
             return (
                 <div className='currentPlayer'>
                     {
                         this.props.playerData ? <h3>{this.props.playerData[0].nickname} its your turn!</h3>:
                         null
                     }
-                {/* {console.log(this.props.playerData)}; */}
+                    {this.props.renderQuestions.length > 0
+                     ? <div>
+                            <h3>{this.props.renderQuestions[this.state.index].question}</h3>
+                            {this.props.renderQuestions[this.state.index].choices.map((choice)=>{
+                                return(
+                                    <button onClick={this.nextQuestion}>{choice}</button>
+                                )
+                            })}
+                     </div>
+                    : null
+                    }   
+                    
                 </div>
+                
             )
         }
     }
